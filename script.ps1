4e0b121f935fee96214c2011dcff59e31c0e7d9d
Sukesh Kotian <sukeshkotiang@gmail.com>
2021-11-21 11:14:04 +0530

    Create script.ps1

---
+++ script.ps1
@@ -0,0 +1,27 @@
+ECHO Installing apps
+
+ECHO Configure chocolatey
+choco feature enable -n allowGlobalConfirmation
+
+choco install notepadplusplus
+choco install vscode
+
+choco install jdk8
+
+choco install filezilla
+choco install winscp.install
+
+choco install putty
+choco install git.install
+
+choco install vlc
+
+choco install winrar
+
+choco install zoom
+
+choco install teamviewer
+
+choco install citrix-workspace
+
+choco install postman
